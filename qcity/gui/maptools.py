d0738f96594c6b77e4535a2c8de2d32b6e226c2f
Nyall Dawson <nyall.dawson@gmail.com>
2025-06-03 09:53:15 +1000

    Cleanup unnecessary members from map tool

--- qcity/gui/maptools.py
+++ qcity/gui/maptools.py
@@ -8,7 +8,6 @@
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 # ---------------------------------------------------------------------
-import os
 from typing import List, Union, Optional
 
 from qgis.PyQt.QtCore import Qt
@@ -16,24 +15,19 @@ from qgis.PyQt.QtGui import QColor
 from qgis.PyQt.QtWidgets import QInputDialog
 from qgis.core import (
     NULL,
-    QgsProject,
     QgsGeometry,
     QgsVectorLayer,
     QgsPointXY,
     QgsRasterLayer,
     Qgis,
     QgsWkbTypes,
-    QgsVectorLayerUtils
 )
 from qgis.gui import (
     QgsMapToolDigitizeFeature,
     QgsMapCanvas,
     QgsAdvancedDigitizingDockWidget,
-    QgsMessageBar,
     QgsMapToolCapture,
     QgsRubberBand,
-    QgsMapTool,
-    QgisInterface,
     QgsAbstractMapToolHandler,
     QgsMapToolCaptureLayerGeometry,
     QgsMapMouseEvent,
@@ -41,7 +35,6 @@ from qgis.gui import (
 )
 
 from qcity.core import LayerType, PROJECT_CONTROLLER, DatabaseUtils
-from qcity.gui.qcity_dock import QCityDockWidget
 
 
 class DrawPolygonTool(QgsMapToolDigitizeFeature):
@@ -52,40 +45,17 @@ class DrawPolygonTool(QgsMapToolDigitizeFeature):
     def __init__(
             self,
             map_canvas: QgsMapCanvas,
-            cad_dock_widget: QgsAdvancedDigitizingDockWidget,
-            message_bar: QgsMessageBar,
-            dlg: QCityDockWidget,
-            iface: QgisInterface,
+            cad_dock_widget: QgsAdvancedDigitizingDockWidget
     ) -> None:
         super().__init__(map_canvas, cad_dock_widget, QgsMapToolCapture.CaptureLine)
         self.default_color: QColor = QColor(255, 0, 0, 100)
         self.cursor_band: Optional[QgsRubberBand] = None
-        self.iface = iface
-        self.dlg = dlg
-        self.unit = None
-        self.cursor_band: Optional[QgsRubberBand] = None
-        self.message_bar: QgsMessageBar = message_bar
-        self.rubber_bands: List[QgsRubberBand] = list()
-        self.last_click_raster_value: Optional[float] = None
-        self.last_map_point: Optional[QgsPointXY] = None
-        self.move_map_point: Optional[QgsPointXY] = None
         self.points: List[QgsPointXY] = []
-        self.project_instance: QgsProject = QgsProject.instance()
-        self.map_canvas: QgsMapCanvas = map_canvas
-        self.last_maptool: QgsMapTool = self.map_canvas.mapTool()
         self.layer: QgsVectorLayer = QgsVectorLayer(
             "Polygon?crs=EPSG:4326", "memory_polygon_layer", "memory"
         )
-        self.project = QgsProject.instance()
-        self.plugin_path = os.path.dirname(os.path.realpath(__file__))
-        self._default_project_area_parameters_path = os.path.join(
-            self.plugin_path, "..", "data", "default_project_area_parameters.json"
-        )
-        self._default_development_site_parameters_path = os.path.join(
-            self.plugin_path, "..", "data", "default_development_site_parameters.json"
-        )
         self.rubber_band = QgsRubberBand(
-            mapCanvas=self.map_canvas,
+            mapCanvas=self.canvas(),
             geometryType=QgsWkbTypes.GeometryType.PolygonGeometry,
         )
         self.snap_indicator = QgsSnapIndicator(map_canvas)
@@ -95,7 +65,7 @@ class DrawPolygonTool(QgsMapToolDigitizeFeature):
     def add_feature(self, layer_type: LayerType, parent_pk):
         self._layer_type = layer_type
         self._parent_pk = parent_pk
-        self.map_canvas.setMapTool(self)
+        self.canvas().setMapTool(self)
 
     def activate(self):
         # skip QgsMapToolDigitizeFeature method -- it has odd logic
@@ -113,7 +83,7 @@ class DrawPolygonTool(QgsMapToolDigitizeFeature):
         """Creates the moving dotted line rubber band."""
         if self.cursor_band:
             self.canvas().scene().removeItem(self.cursor_band)
-        self.cursor_band = QgsRubberBand(self.map_canvas, QgsWkbTypes.PolygonGeometry)
+        self.cursor_band = QgsRubberBand(self.canvas(), QgsWkbTypes.PolygonGeometry)
         self.cursor_band.setColor(self.default_color)
         self.cursor_band.setLineStyle(Qt.DotLine)
 
@@ -124,7 +94,7 @@ class DrawPolygonTool(QgsMapToolDigitizeFeature):
 
     def createRubberBand(self):
         """Builds rubber band from all points and adds it to the map canvas."""
-        self.rubber_band = QgsRubberBand(self.map_canvas, QgsWkbTypes.PolygonGeometry)
+        self.rubber_band = QgsRubberBand(self.canvas(), QgsWkbTypes.PolygonGeometry)
         self.rubber_band.setColor(self.default_color)
 
         for point in self.points:
