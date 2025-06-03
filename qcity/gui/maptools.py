f30f31251fcc45ea0fc6cd25e4a0a33ca738ea98
Nyall Dawson <nyall.dawson@gmail.com>
2025-06-03 10:43:54 +1000

    Allow use of CAD dock and other advanced tools when digitizing
    Requires QGIS 3.44+, as we need the upstream fix
    https://github.com/qgis/QGIS/pull/62097
    
    Fixes #2

--- qcity/gui/maptools.py
+++ qcity/gui/maptools.py
@@ -37,9 +37,13 @@ from qgis.gui import (
 from qcity.core import LayerType, PROJECT_CONTROLLER, DatabaseUtils
 
 
-class DrawPolygonTool(QgsMapToolDigitizeFeature):
+class DrawPolygonToolOld(QgsMapToolDigitizeFeature):
     """
-    A map tool for drawing polygons
+    A map tool for drawing polygons, for use with QGIS < 3.44.
+
+    Does NOT support CAD digitizing dock, as we need https://github.com/qgis/QGIS/pull/62097 for that
+
+    Remove when we can run on 3.44+ only.
     """
 
     def __init__(
@@ -197,6 +201,88 @@ class DrawPolygonTool(QgsMapToolDigitizeFeature):
         if self.cursor_band:
             self.canvas().scene().removeItem(self.cursor_band)
         self.cursor_band = None
+
+
+class DrawPolygonTool(QgsMapToolCaptureLayerGeometry):
+    """
+    A map tool for drawing polygons, for use with QGIS 3.44+
+
+    Does support CAD digitizing dock.
+    """
+
+    def __init__(
+            self,
+            map_canvas: QgsMapCanvas,
+            cad_dock_widget: QgsAdvancedDigitizingDockWidget
+    ) -> None:
+        super().__init__(map_canvas, cad_dock_widget, QgsMapToolCapture.CapturePolygon)
+        self.points: List[QgsPointXY] = []
+        self._temp_layer: QgsVectorLayer = QgsVectorLayer(
+            "Polygon?crs=EPSG:4326", "memory_polygon_layer", "memory"
+        )
+        self._layer_type = LayerType.ProjectAreas
+        self._parent_pk = NULL
+
+    def layer(self):
+        return self._temp_layer
+
+    def add_feature(self, layer_type: LayerType, parent_pk):
+        self._layer_type = layer_type
+        self._parent_pk = parent_pk
+        self.canvas().setMapTool(self)
+
+    def layerGeometryCaptured(self, polygon: QgsGeometry) -> None:
+        if polygon.constGet().exteriorRing().numPoints() < 3:
+            return
+
+        feature_name, ok = QInputDialog.getText(
+            self.canvas(), self.tr("Create {}").format(self._layer_type.as_title_case(plural=False)),
+            self.tr("Input {} name").format(self._layer_type.as_sentence_case(plural=False)))
+
+        if not ok:
+            return
+
+        self.create_feature(
+            feature_name, polygon
+        )
+
+    def create_feature(self, feature_name: str, polygon: QgsGeometry) -> None:
+        """
+        Called when the new feature should be created
+        """
+        layer = PROJECT_CONTROLLER.get_layer(self._layer_type)
+
+        existing_names = PROJECT_CONTROLLER.get_unique_names(self._layer_type)
+        feature_name_exists = feature_name in existing_names
+
+        if not feature_name or feature_name_exists:
+            if feature_name_exists:
+                # TODO: Message bar here instead.
+                print("Table name already exists.")
+            return
+
+        if not layer.isValid():
+            raise Exception("Layer is not valid!")
+
+        initial_attributes = {}
+        foreign_key = DatabaseUtils.foreign_key_for_layer(self._layer_type)
+        if foreign_key:
+            initial_attributes[foreign_key] = self._parent_pk
+        if self._layer_type == LayerType.BuildingLevels:
+            initial_attributes["level_index"] = PROJECT_CONTROLLER.get_next_building_level(
+                self._parent_pk)
+            initial_attributes["base_height"] = PROJECT_CONTROLLER.get_floor_base_height(
+                self._parent_pk, initial_attributes["level_index"])
+
+        feature = PROJECT_CONTROLLER.create_feature(self._layer_type,
+                                                    feature_name,
+                                                    polygon,
+                                                    initial_attributes)
+
+        layer.startEditing()
+        layer.addFeature(feature)
+        layer.commitChanges()
+
 
 
 class MapToolHandler(QgsAbstractMapToolHandler):
