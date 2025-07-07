985a52e076d8092f68e975bce6c2c4d64aa905ba
Nyall Dawson <nyall.dawson@gmail.com>
2025-07-07 13:16:18 +1000

    Add documentation workflows

---
+++ docs/src/.vuepress/sidebar/en.js
@@ -0,0 +1,27 @@
+module.exports = {
+      '/': [
+        {
+            title: 'Get started',
+            collapsable: false,
+            sidebarDepth: 0,
+            children: [
+                '/user_guide/pricing',
+                '/user_guide/installation',
+                '/user_guide/configuring',
+            ]
+        },
+        '/user_guide/tools',
+        {
+            title: 'Support',
+            collapsable: false,
+            sidebarDepth: 0,
+            children: [
+                 '/user_guide/faq',
+                 '/user_guide/troubleshooting',
+                 '/user_guide/limitations',
+                 '/user_guide/changelog',
+                 '/user_guide/about',
+            ]
+        }
+      ]
+}
---
+++ docs/src/user_guide/about.md
@@ -0,0 +1,3 @@
+# About QCity
+
+Something
